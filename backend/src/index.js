13ff11ecf6b62ce41c43bb5c397d9e756f39dca1
UnqKode <mycodeunq@gmail.com>
2025-02-17 15:45:56 +0530

    project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.routes.js"
 import cors from "cors"
 import { app , server } from './lib/socket.js'
+import path from "path"
 
 dotenv.config()
 const port = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json());//to grab the body
 app.use(cookieParser());//to grab cookies
@@ -20,6 +22,13 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"/frontend","dist","index.html"))
+    })
+}
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
