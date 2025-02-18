1cf93f3acdcf42893a1134477468035a59141f04
UnqKode <mycodeunq@gmail.com>
2025-02-18 15:54:06 +0530

    Home chnages

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useChatStore } from '../store/useChatStore'
 import NoChatSelected from '../components/NoChatSelected'
 import Sidebar from '../components/SideBar'
 import ChatContainer from '../components/ChatContainer'
+import { motion } from 'framer-motion'
 
 function HomePage() {
 
@@ -10,14 +11,14 @@ function HomePage() {
 
   return (
     <div className='h-screen bg-base-200'>
-      <div className='flex items-center justify-center pt-20 px-4'>
+      <motion.div initial={{ scale: 0  }} animate={{ scale: 1 }} transition={{ duration: 0.3, ease: "easeOut" }} className='flex items-center justify-center pt-20 px-4'>
         <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
           <div className='flex h-full rounded-lg overflow-hidden'>
             <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
-          </div>
         </div>
       </div>
+          </motion.div>
     </div>
   )
 }
